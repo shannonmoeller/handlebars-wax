8bcdc9f6e6ae60db41020e04243a1bcba39585e8
Shannon Moeller <me@shannonmoeller.com>
2015-05-05 11:34:49 -0400

    Updated node_js versions in travis. Updated doc blocks. Removed unused method.

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ function registerModule(handlebars, method, obj, mod) {
  *
  * @type {Function}
  * @param {Object} handlebars Handlebars instance.
- * @param {Object} options Plugin options.
- * @param {Boolean} options.bustCache Whether to force the reload of a module by deleting it from the cache.
- * @param {String} options.cwd Current working directory. Defaults to `process.cwd()`.
- * @param {Object|String|Array.<String>|Function} options.helpers One or more glob strings matching helpers.
- * @param {Function} options.helpersReducer Custom reducer for registering helpers.
- * @param {Object|String|Array.<String>|Function} options.partials One or more glob strings matching partials.
- * @param {Function} options.partialsReducer Custom reducer for registering partials.
+ * @param {Object=} options Plugin options.
+ * @param {Boolean=} options.bustCache Whether to force the reload of modules by deleting them from the cache.
+ * @param {String=} options.cwd Current working directory. Defaults to `process.cwd()`.
+ * @param {Object|String|Array.<String>|Function=} options.helpers One or more glob strings matching helpers.
+ * @param {Function=} options.helpersReducer Custom reducer for registering helpers.
+ * @param {Object|String|Array.<String>|Function=} options.partials One or more glob strings matching partials.
+ * @param {Function=} options.partialsReducer Custom reducer for registering partials.
  * @return {Object} Handlebars instance.
  */
 function registrar(handlebars, options) {
@@ -50,8 +50,10 @@ function registrar(handlebars, options) {
 	var helpers = options.helpers,
 		partials = options.partials;
 
+	// Expose handlebars to custom reducers
 	options.handlebars = handlebars;
 
+	// Register helpers
 	if (helpers) {
 		options.reducer = options.helpersReducer || (
 			registerModule.bind(null, handlebars, 'registerHelper')
@@ -60,6 +62,7 @@ function registrar(handlebars, options) {
 		requireGlob.sync(helpers, options);
 	}
 
+	// Register partials
 	if (partials) {
 		options.reducer = options.partialsReducer || (
 			registerModule.bind(null, handlebars, 'registerPartial')
@@ -70,15 +73,5 @@ function registrar(handlebars, options) {
 
 	return handlebars;
 }
-
-/**
- * Assemble-compatible register method.
- *
- * @method register
- * @param {Object} handlebars Handlebars instance.
- * @return {Object} Handlebars instance.
- * @static
- */
-registrar.register = registrar;
 
 module.exports = registrar;
